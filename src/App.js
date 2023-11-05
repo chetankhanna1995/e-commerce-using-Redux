a867957facdef2341561b07d8c78bbf2602e4601
chetankhanna1995 <chetankhanna1995@gmail.com>
2023-11-05 21:47:57 +0530

    allign footer and card

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import store from "./store/store";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
-
 function App() {
   return (
     <div className="App container-xl mx-16">
-      <Provider store={store} >
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route></Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
-     
     </div>
   );
 }
